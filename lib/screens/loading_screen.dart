a8885996f5ca413e5a69b5732e669863bb003b78
GarifullinII <ildaridrisovich@gmail.com>
2023-04-25 01:03:34 +0400

    change response

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:climate_data/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -9,14 +11,26 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
   void _getLocation() async {
-    await Geolocator.checkPermission();
-    await Geolocator.requestPermission();
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
 
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
+  void getData() async {
+    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f24b480465f1f9fbe5c3bedfcead602'));
 
-    print(position);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+
+      var longitude = jsonDecode(data)['coord']['lon'];
+      print(longitude);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -25,7 +39,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            _getLocation();
+            // _getLocation();
+            getData();
           },
           child: const Text('GET LOCATION'),
         ),
